853b9f38468ca8e6a4814501fb15deaf7a229083
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-10-14 23:07:07 +0900

    Rebase from 'debian/melodic/openrtm_aist_python'

--- setup.py
+++ setup.py
@@ -552,8 +552,7 @@ try:
                                       "sdist":OtherSetupForSdist, "install":Install, "install_data":InstallData },
                  packages         = unix_packages,
                  scripts= ['OpenRTM_aist/utils/rtcprof/rtcprof_python',
-                           'OpenRTM_aist/utils/rtcd/rtcd_python',
-                           'OpenRTM_aist/ext/sdo/observer/setup.sh'],
+                           'OpenRTM_aist/utils/rtcd/rtcd_python'],
                  data_files       = unix_data_files)
       
     # for RTM zip (sdist)
