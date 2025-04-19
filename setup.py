51db5682f658db7a50abb6b017df4d5661e06d5c
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2025-04-19 16:02:54 +0900

    'string' does not support 'join'

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ def compile_idl(cmd, pars, files):
   Put together command line for python stubs generation.
   """
   global g_os
-  cmdline = cmd +' '+ string.join(pars) +' '+string.join(files)
+  cmdline = cmd +' '+ " ".join(pars) +' '+" ".join(files)
   if g_os == "win32":
     os.system(cmdline)
     return
